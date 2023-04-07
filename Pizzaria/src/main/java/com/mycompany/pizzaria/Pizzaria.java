ff9766758b3fcb10ad924f73373269e78d146660
MarvinK1 <130174128+MarvinK1@users.noreply.github.com>
2023-04-07 14:17:45 -0300

    Update Pizzaria.java

--- Pizzaria/src/main/java/com/mycompany/pizzaria/Pizzaria.java
+++ Pizzaria/src/main/java/com/mycompany/pizzaria/Pizzaria.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.pizzaria;
 
-/**
- *
- * @author marqu
- */
 public class Pizzaria {
     public static void main(String[] args) {
         Cliente cliente = new Cliente("João", "Rua A, 123");
