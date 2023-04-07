7cab1b4e421ebf9516058d4da41fe94ebd4ea558
MarvinK1 <130174128+MarvinK1@users.noreply.github.com>
2023-04-07 14:17:24 -0300

    Update Pedido.java

--- Pizzaria/src/main/java/com/mycompany/pizzaria/Pedido.java
+++ Pizzaria/src/main/java/com/mycompany/pizzaria/Pedido.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.pizzaria;
 import java.util.ArrayList;
 
-/**
- *
- * @author marqu
- */
 public class Pedido {
     private Cliente cliente;
     private ArrayList<Comida> comidas;
